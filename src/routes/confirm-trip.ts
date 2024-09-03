8a528bf1bfe1e4d9bee120363b8f199490fc0edd
gabrielalmir <gabr.almir@gmail.com>
2024-09-03 18:52:44 -0300

    feat: add confirm trip

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import nodemailer from 'nodemailer';
 import z from "zod";
+import { dayjs } from "../lib/dayjs";
+import { getMailClient } from "../lib/mail";
+import { prisma } from "../lib/prisma";
 
 const ConfirmTripSchema = {
     params: z.object({
@@ -9,8 +13,56 @@ const ConfirmTripSchema = {
 };
 
 export async function confirmTrip(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post('/trips/:tripId/confirm', { schema: ConfirmTripSchema }, async (request, reply) => {
+    app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', { schema: ConfirmTripSchema }, async (request, reply) => {
         const { tripId } = request.params;
-        return { tripId }
+
+        const trip = await prisma.trip.findUnique({
+            where: { id: tripId },
+            include: { participants: { where: { is_owner: false } } }
+        });
+
+        if (!trip) {
+            return reply.callNotFound();
+        }
+
+        if (trip.is_confirmed) {
+            return reply.redirect(`http://localhost:3000/trips/${tripId}`);
+        }
+
+        await prisma.trip.update({
+            where: { id: tripId },
+            data: { is_confirmed: true }
+        });
+
+        const formattedStartDate = dayjs(trip.starts_at).format('LL');
+        const formattedEndDate = dayjs(trip.ends_at).format('LL');
+
+        const mail = await getMailClient();
+
+        await Promise.all(trip.participants.map(async participant => {
+            const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/${participant.id}`;
+            const message = await mail.sendMail({
+                from: { name: 'Equipe Planner', address: 'hi@plann.er' },
+                to: participant.email,
+                subject: `Planner - Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
+                html: `
+                    <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+                        <p>Você foi convidado para participar de uma viagem para <strong>${trip.destination}</strong>, nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+                        <p></p>
+                        <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+                        <p></p>
+                        <p>
+                            <a href="${confirmationLink}">Confirmar Viagem</a>
+                        </p>
+                        <p></p>
+                        <p>Caso você não saiba do que se trata esse e-mail, apenas o ignore.</p>
+                    </div>
+                `.trim()
+            });
+
+            console.log(nodemailer.getTestMessageUrl(message));
+        }))
+
+        return reply.redirect(`http://localhost:3000/trips/${tripId}`);
     });
 }
